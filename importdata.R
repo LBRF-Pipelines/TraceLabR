910353beecf55a17c0fd8ecbca75a186ed5b86e0
Tony Ingram <tony.g.ingram@gmail.com>
2016-07-20 12:27:30 -0300

    just a joke

--- importdata.R
+++ importdata.R
@@ -56,9 +56,7 @@ rotation <- acos(trans$gamm[1,1])
 # -asin(trans$gamm[1,2])
 # acos(trans$gamm[2,2])
 
-#test for github 
-
-# HERE IS A BUNCH OF NEW PROCRUSTES STUFF! blah blah balls change 
+# we're not gonna screw up that bad.. yet... 
 
 
 
