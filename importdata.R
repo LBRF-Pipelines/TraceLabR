9ef9aba214a8a56d5e6510d2e52c7405b53b0ee7
Tony Ingram <tony.g.ingram@gmail.com>
2016-08-31 16:31:49 -0300

    Added complexity to all_data — looks like it works. Although there's a new "NAs introduced by coercion" warning...

--- importdata.R
+++ importdata.R
@@ -34,7 +34,7 @@ for(i in 1:length(file.names)) {
         # separate PP data from MI and CC data (remember, final session of MI and CC are also PP sessions)
         if (length(tlt)<15){
                 # disclude all groups except CC
-                if(trials[trials$figure_file==name.tlf,5]!='CC-00-5'){datarow=c(name.tlf,rep(NA,times=10))}
+                if(trials[trials$figure_file==name.tlf,5]!='CC-00-5'){datarow=c(name.tlf,rep(NA,times=11))}
                 # if in CC group, runs control task
                 else{
                         #loads stimulus data
@@ -207,25 +207,26 @@ for(i in 1:length(file.names)) {
                 
                 ##### save variables to a row & subsequently a file #####
                 
-                datarow <- c(name.tlf,PLstim,PLresp,RawSS,RawSD,translation,scale,rotation,ProcSS,ProcSD,rep(NA,times=1))
+                datarow <- c(name.tlf,PLstim,complexity,PLresp,RawSS,RawSD,translation,scale,rotation,ProcSS,ProcSD,rep(NA,times=1))
         }
         out.file <- rbind(out.file, datarow)
 }
 
 #change output to df
 df.out.file <- data.frame(out.file[-1,],stringsAsFactors = FALSE)
-colnames(df.out.file) <- c("figure_file","PLstim","PLresp","RawSS","RawSD","translation","scale","rotation","ProcSS","ProcSD","correct_response")
+colnames(df.out.file) <- c("figure_file","PLstim","complexity","PLresp","RawSS","RawSD","translation","scale","rotation","ProcSS","ProcSD","correct_response")
 
 #combine proc_df with db
 all_data <- merge(trials,df.out.file,by="figure_file")
 colnames(participants)[1] <- paste("participant_id")
 all_data <- merge(participants[,c(1,4:6)],all_data,by="participant_id")
-all_data <- all_data[c("participant_id","sex","age","handedness","condition","session_num","block_num","trial_num","figure_type","figure_file","stimulus_gt","stimulus_mt","avg_velocity","path_length","PLstim","trace_file","rt","it","mt","PLresp","RawSS","RawSD","translation","scale","rotation","ProcSS","ProcSD","control_question","control_response","correct_response")]
+all_data <- all_data[c("participant_id","sex","age","handedness","condition","session_num","block_num","trial_num","figure_type","figure_file","stimulus_gt","stimulus_mt","avg_velocity","path_length","PLstim","complexity","trace_file","rt","it","mt","PLresp","RawSS","RawSD","translation","scale","rotation","ProcSS","ProcSD","control_question","control_response","correct_response")]
 
 #change data to numeric where appropriate
 all_data$condition <- as.factor(all_data$condition)
 all_data$figure_type <- as.factor(all_data$figure_type)
 all_data$PLstim <- as.numeric(all_data$PLstim)
+all_data$complexity <- as.numeric(all_data$complexity)
 all_data$PLresp <- as.numeric(all_data$PLresp)
 all_data$RawSS <- as.numeric(all_data$RawSS)
 all_data$RawSD <- as.numeric(all_data$RawSD)
