066763ddd450b52f384684e9ef3bf6c85d14c972
Tony Ingram <tony.g.ingram@gmail.com>
2016-10-07 16:08:34 -0300

    calculation for total curvature

--- complexity.R
+++ complexity.R
@@ -3,34 +3,54 @@
 graphics.off() # clear figures
 #cat("\014") # clear console
 
-### create parametric equation for figure ###
+#plot figure:
+#plot(data_stim$X1,data_stim$X2, xlim=c(0,1920), ylim=c(1080,0)) 
+
+### using parametric equation method ###
+
+# first normalize to 5000 time points: 
+# (because I don't know how to just get derivatives of splines themselves...)
+time <- seq(min(data_stim$X3), max(data_stim$X3), length.out = 5000) #but this takes a long time
+#arclength <- seq(0, pathlength, length.out = 5000)
+# NOTE: normalizing time points to a particular number (e.g. 100) should be equivalent
+# to normalizing by arclength, because the animation moves at a constant velocity. RIGHT?
 
 # make two functions x(t) and y(t):
 
-plot(data_stim$X3,data_stim$X1) #x(t)
+#plot(data_stim$X3,data_stim$X1) #x(t)
 xt.spl <- smooth.spline(x = data_stim$X3, y = data_stim$X1, df = (.5*nrow(data_stim)))
-lines(xt.spl)
+#lines(xt.spl)
+dxdt <- predict(xt.spl, x = time, deriv = 1) # first derivative of x
+#plot(dxdt)
+d2xdt2 <- predict(xt.spl, x = time, deriv = 2) # second derivative of x
+#plot(d2xdt2)
 
-plot(data_stim$X3,data_stim$X2) #y(t)
+#plot(data_stim$X3,data_stim$X2) #y(t)
 yt.spl <- smooth.spline(x = data_stim$X3, y = data_stim$X2, df = (.5*nrow(data_stim)))
-lines(yt.spl)
+#lines(yt.spl)
+dydt <- predict(yt.spl, x = time, deriv = 1) # first derivative of y
+#plot(dydt)
+d2ydt2 <- predict(yt.spl, x = time, deriv = 2) # second derivative of y
+#plot(d2ydt2)
 
-plot(data_stim$X1,data_stim$X2, xlim=c(0,1920), ylim=c(1080,0))
-lines(path.spl)
+# calculate curvature:
 
-predictx <- predict(path.spl, x = xy[,1])
-predicty <- predict(path.spl, x = xy[,2])
+curvature = (dxdt$y*d2ydt2$y - dydt$y*d2xdt2$y)/((dxdt$y^2 + dydt$y^2)^(3/2))
+plot(time, curvature, ylim = c(-.01,.01))
 
-plot(predictx$x, predictx$y)
-plot(predicty$x, predicty$y)
-plot(predictx$x, predicty$y)
+# calculate total curvature:
 
-plot(predictx$x,xy[,2])
-plot(predictx$x,xy[,1])
+curvature.spl <- splinefun(time, curvature)
 
-plot(predicty$x,xy[,2])
+totcurv <- integrate(curvature.spl, lower = min(time), upper = max(time))
 
+complexity2 <- totcurv$value
 
-points(predicty$x,xy[,2], col="red")
+# note: using this method, the magnitude of peaks and valleys in curvature 
+# are a function of how smooth you make the splines, such that better fitting
+# leads to bigger spikes in curvature at the vertices of the figure.
+# suggestion: test the final answer (integral) using a few different df's.
 
+# INDEED: once you get beyond a couple thousand points, the curvature doesn't change much, 
+# and once you get to < 20 df, the curvature score doesn't change much.
 
