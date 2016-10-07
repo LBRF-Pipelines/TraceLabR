f0164669d9f6f1fcafcebbfb3be478d1c1c45a67
Tony Ingram <tony.g.ingram@gmail.com>
2016-10-07 12:02:34 -0300

    playing around with complexity measures using splines etc.

--- complexity.R
+++ complexity.R
@@ -1,7 +1,7 @@
 # complexity workspace 
 
 xy <- matrix(c(data_stim$X1,data_stim$X2),ncol=2)
-path.spl <- smooth.spline(xy, y = NULL, df = 20)
+path.spl <- smooth.spline(xy, y = NULL, df = 30)
 plot(data_stim$X1,data_stim$X2, xlim=c(0,1920), ylim=c(1080,0))
 lines(path.spl)
 
@@ -19,3 +19,5 @@ plot(predicty$x,xy[,2])
 
 
 points(predicty$x,xy[,2], col="red")
+
+
