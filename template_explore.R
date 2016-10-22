ef610d0bf2d02df96b31c624818eb1c0983076b4
Tony Ingram <tony.g.ingram@gmail.com>
2016-10-22 17:21:06 -0300

    New file for exploring and analyzing the data imported and processed by template_analysis.R. This script will be used to explore the characteristics of figures randomly generated at particular settings given their bezier curve control points, and as such, their raw data. This will be used to pick repeated figures that fall close to the median pathlength and complexity.

---
+++ template_explore.R
@@ -0,0 +1,12 @@
+##### Template Figure Analysis #####
+  ### authored by Tony Ingram ###
+
+# this script explores the template figures imported and processed with the 
+# template_analysis.R script.
+
+rm(list=setdiff(ls(), c("all_figs","all_data"))) # clear all but all_figs & all_data
+# graphics.off() # clear figures
+# cat("\014") # clear console
+
+
+
