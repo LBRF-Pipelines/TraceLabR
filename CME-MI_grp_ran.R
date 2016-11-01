678db35fe154e43e6eb651f0c1384cda3206b78e
Tony Ingram <tony.g.ingram@gmail.com>
2016-11-01 09:46:17 -0300

    creating a script for randomizing group assignment

---
+++ CME-MI_grp_ran.R
@@ -0,0 +1,10 @@
+##### Randomizing Group Assignment #####
+
+#### Nov 1st, 2016 ####
+# not including groups occluding visual feedback of arm
+# because mirroring function of paradigm not working yet.
+
+library(tidyverse)
+
+set.seed(1)
+
