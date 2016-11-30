0cbfdfdfcf2688ea7b809184174ff7dbfd6760ec
Tony Ingram <tony.g.ingram@gmail.com>
2016-11-30 14:27:40 -0400

    copied more script from the class, need to edit it quite a lot...

--- CME-MI_stats.R
+++ CME-MI_stats.R
@@ -5,16 +5,38 @@
 library(tidyverse)
 library(ez)
 library(rstan)
+rstan_options(auto_write = TRUE)
 
 rm(list=setdiff(ls(), c("all_figs","all_data"))) # clear all
 # graphics.off() # clear figures
 # cat("\014") # clear console
 
-###### ######
+# load and look at data ------------
 
 load("all_data.Rda")
 
-## data for stan ##
 
+# pre-compile the models that we'll use -----------------------------------
+cme = rstan::stan_model('CME-MI_stats.stan')
+
+
+# Sample the model ---------
+data_for_stan = list(
+        N = nrow(dat)
+        , K = ncol(dat)-1
+        , X = scale(dat[,1:2]) #trick to standardize
+        , Y = scale(dat[,3])[,1] #trick to standardize
+)
+post = rstan::sampling(
+        object = cme
+        , data = data_for_stan
+        , seed = 1
+        , cores = 4
+)
+print(
+        post
+        , probs = c(.025,.975)
+        , digits = 2
+)
 
 
