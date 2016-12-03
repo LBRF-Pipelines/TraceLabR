26639b269a89e372192db111c50adc2cbe432575
Tony Ingram <tony.g.ingram@gmail.com>
2016-12-03 09:58:28 -0400

    Changed filtering criteria for weird values... need a way to deal with these in a principled manner.

--- SAF_compare_ran.R
+++ SAF_compare_ran.R
@@ -13,10 +13,11 @@ load("all_data.Rda")
 
 all_data_sub <- dplyr::filter(
         .data = all_data
-        , vresp > 200
-        , scale < 2
-        , rotation < 1
-        , translation < 400
+        , raw_dtw_error_mean < 400
+        # , vresp > 200
+        # , scale < 2
+        # , rotation < 1
+        # , translation < 400
 ) # note: this removes all MI and CC trials, so do next two lines to see how many dropped:
 aggregate(!is.na(PLresp) ~ stimulus_gt, all_data, sum)
 aggregate(!is.na(PLresp) ~ stimulus_gt, all_data_sub, sum)
