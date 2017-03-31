a64b1eab0e7f7c36adb7503e81b4a52c3fb2b9e6
Tony Ingram <tony.g.ingram@gmail.com>
2017-03-31 09:14:25 -0300

    changing participant number

--- SAF_p.R
+++ SAF_p.R
@@ -13,7 +13,7 @@ load("all_data.Rda")
 
 all_data_sub <- dplyr::filter(
         .data = all_data
-        , shape_dtw_error_mean < 400
+        # , shape_dtw_error_mean < 400
         # , vresp > 200
         # , scale < 2
         # , rotation < 1
@@ -25,7 +25,7 @@ aggregate(!is.na(PLresp) ~ stimulus_gt, all_data_sub, sum)
 
 ## SET UP PARTICIPANT ##
 # what participant?
-p <- 56
+p <- 62
 #fig <- dplyr::filter(all_data, participant_id == p, figure_type != "random")$figure_type[1] # can't remember why I did this... old code I guess
 
 
